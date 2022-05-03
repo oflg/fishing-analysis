17c6698ec82a87e1a20fa026e14fdca5cbbecd83
oflg <oflg@foxmail.com>
2022-05-03 22:53:02 +0800

    fix: performance

--- calender/calendar.js
+++ calender/calendar.js
@@ -4,7 +4,7 @@ type: application/javascript
 module-type: echarts-component
 Calendar for Fishing.
 \*/
-exports.shouldUpdate = "[{$:/temp/fishing!!list}]";
+exports.shouldUpdate = "[{$:/temp/fishing!!year}]";
 
 exports.onUpdate = function (myChart) {
     var calendarLan = $tw.wiki.filterTiddlers("[{$:/language/fishing/calendar}]")[0],
